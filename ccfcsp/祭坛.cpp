5562aec894e2c7448c8ce3816ece9ec9704013b9
Linna <1500012782@pku.edu.cn>
2017-12-02 15:23:10 +0800

    Finish 201709-4.

--- ccfcsp/祭坛.cpp
+++ ccfcsp/祭坛.cpp
@@ -1,124 +1,135 @@
-//201609-5
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-class node
-{
-public:
-	int id;
-	// bool isCol;  // isCol=true表示是一行，否则是一列
-	vector<int> list;
-	node(int inid = 0)
-	{
-		id = inid;
-	}
-	bool operator < (node b)
-	{
-		return list.size() > b.list.size();
-	}
-};
-vector<node> Col, Row;
-//把(x,y)这个点插入体系中
-void Insert(int x, int y)
-{
-	int colsize = Col.size();
-	bool findcol = false;
-	for (int i = 0; i < colsize; i++)
-	{
-		if (Col[i].id == x)
-		{
-			Col[i].list.push_back(y);
-			// sort(Col[i].list.begin(), Col[i].list.end());
-			findcol = true;
-			break;
-		}
-	}
-	if (!findcol)
-	{
-		node new_node(x);
-		new_node.list.push_back(y);
-		Col.push_back(new_node);
-	}
-	sort(Col.begin(), Col.end());
-
-	int rowsize = Row.size();
-	bool findrow = false;
-	for (int i = 0; i < rowsize; i++)
-	{
-		if (Row[i].id == y)
-		{
-			Row[i].list.push_back(x);
-			// sort(Row[i].list.begin(), Row[i].list.end());
-			findrow = true;
-			break;
-		}
-	}
-	if (!findrow)
-	{
-		node new_node(y);
-		new_node.list.push_back(x);
-		Row.push_back(new_node);
-	}
-	sort(Row.begin(), Row.end());
-}
-
-//Col[colid]和Row[rowid]能够产生几层
-int Search(int colid, int rowid)
-{
-	int x = Col[colid].id, y = Row[rowid].id;
-	int colsize = Col[colid].list.size();
-	int rowsize = Row[rowid].list.size();
-	int belowy = 0, abovey = 0, belowx = 0, abovex = 0;
-	for (int i = 0; i < colsize; i++)
-	{
-		if (Col[colid].list[i] == y) return 0;
-		if (Col[colid].list[i] < y) belowy++;
-		if (Col[colid].list[i] > y) abovey++;
-	}
-	for (int i = 0; i < rowsize; i++)
-	{
-		if (Row[rowid].list[i] == x) return 0;
-		if (Row[rowid].list[i] < x) belowx++;
-		if (Row[rowid].list[i] > x) abovex++;
-	}
-	int result = min(belowx, abovex);
-	result = min(result, belowy);
-	result = min(result, abovey);
-	return result;
-}
-int main()
-{
-	int n = 0, q = 0;
-	int x = 0, y = 0;
-	int result = 0, tempresult = 0, resultnum = 0;
-	cin >> n >> q;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> x >> y;
-		Insert(x, y);
-	}
-	//找最大的层数
-	for (int i = 0; i < Col.size(); i++)
-	{
-		for (int j = 0; j < Row.size(); j++)
-		{
-			if ((Col[i].list.size() >= 2 * result) && (Row[j].list.size() >= 2 * result))
-			{
-				tempresult = Search(i, j);
-				if (tempresult > result)
-				{
-					result = tempresult;
-					resultnum = 1;
-				}
-				else if (tempresult == result)
-				{
-					resultnum++;
-				}
-			}
-		}
-	}
-	if (q == 1) cout << result << endl;
-	else if (q == 2) cout << resultnum << endl;
-	return 0;
-}
+////201609-5
+//// 60%
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//class node
+//{
+//public:
+//	int id;
+//	vector<int> list;
+//	node(int inid = 0)
+//	{
+//		id = inid;
+//	}
+//	bool operator < (node b)
+//	{
+//		return list.size() > b.list.size();
+//	}
+//};
+//vector<node> Col, Row;
+////把(x,y)这个点插入体系中
+//void Insert(int x, int y)
+//{
+//	int colsize = Col.size();
+//	bool findcol = false;
+//	for (int i = 0; i < colsize; i++)
+//	{
+//		if (Col[i].id == x)
+//		{
+//			Col[i].list.push_back(y);			
+//			findcol = true;
+//			break;
+//		}
+//	}
+//	if (!findcol)
+//	{
+//		node new_node(x);
+//		new_node.list.push_back(y);
+//		Col.push_back(new_node);
+//	}	
+//
+//	int rowsize = Row.size();
+//	bool findrow = false;
+//	for (int i = 0; i < rowsize; i++)
+//	{
+//		if (Row[i].id == y)
+//		{
+//			Row[i].list.push_back(x);			
+//			findrow = true;
+//			break;
+//		}
+//	}
+//	if (!findrow)
+//	{
+//		node new_node(y);
+//		new_node.list.push_back(x);
+//		Row.push_back(new_node);
+//	}	
+//}
+////找出vector<int>中比mid小的数的个数，用二分
+//int findLower(vector<int> list, int mid)
+//{
+//	sort(list.begin(), list.end());
+//	vector<int>::iterator it=find(list.begin(),list.end(),mid);
+//	if (it != list.end()) return -1;
+//}
+////Col[colid]和Row[rowid]能够产生几层
+//int Search(int colid, int rowid)
+//{
+//	int x = Col[colid].id, y = Row[rowid].id;	
+//	int resultrow = 0, resultcol = 0;
+//
+//	/*
+//	int findCol = findLower(Col[colid].list, y);
+//	if (findCol == -1) return 0;
+//	resultcol = min(findCol, (int)Col[colid].list.size() - findCol);
+//	*/
+//
+//	int colsize = Col[colid].list.size();
+//	int rowsize = Row[rowid].list.size();
+//	int belowy = 0, abovey = 0, belowx = 0, abovex = 0;
+//	for (int i = 0; i < colsize; i++)
+//	{
+//		if (Col[colid].list[i] == y) return 0;
+//		if (Col[colid].list[i] < y) belowy++;
+//		if (Col[colid].list[i] > y) abovey++;
+//	}	
+//	for (int i = 0; i < rowsize; i++)
+//	{
+//		if (Row[rowid].list[i] == x) return 0;
+//		if (Row[rowid].list[i] < x) belowx++;
+//		if (Row[rowid].list[i] > x) abovex++;
+//	}
+//	resultrow = min(belowx, abovex);
+//	resultcol = min(belowy, abovey);
+//	return min(resultrow, resultcol);
+//}
+//int main()
+//{
+//	int n = 0, q = 0;
+//	int x = 0, y = 0;
+//	int result = 0, tempresult = 0, resultnum = 0;
+//	cin >> n >> q;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> x >> y;
+//		Insert(x, y);
+//	}
+//	sort(Col.begin(), Col.end());
+//	sort(Row.begin(), Row.end());
+//	//找最大的层数
+//	for (int i = 0; i < Col.size(); i++)
+//	{
+//		for (int j = 0; j < Row.size(); j++)
+//		{
+//			if ((Col[i].list.size() >= 2 * result) && (Row[j].list.size() >= 2 * result))
+//			{
+//				tempresult = Search(i, j);
+//				if (tempresult > result)
+//				{
+//					result = tempresult;
+//					resultnum = 1;
+//				}
+//				else if (tempresult == result)
+//				{
+//					resultnum++;
+//				}
+//			}
+//		}
+//	}
+//	if (q == 1) cout << result << endl;
+//	else if (q == 2) cout << resultnum << endl;
+//	return 0;
+//}
--- ccfcsp/通信网络.cpp
+++ ccfcsp/通信网络.cpp
@@ -1,94 +1,156 @@
-//// TODO
-//// TLE
-//#include <iostream>
+////// 先是我的 TLE 版本
+////// 下面是网上的一个能过的解~
+////#include <iostream>
+////#include <stdio.h>
+////#include <vector>
+////#include <algorithm>
+////using namespace std;
+////int dist[1010][1010] = { 0 };
+////struct node
+////{
+////	vector<int> inlist;
+////	vector<int> outlist;
+////};
+////int main()
+////{
+////	int N = 0, M = 0;
+////	int a = 0, b = 0;
+////	int result = 0;
+////	int temp = 0;
+////
+////	cin >> N >> M;
+////	node nodelist[1010];
+////	for (int i = 1; i <= N; i++)
+////	{
+////		for (int j = 1; j <= N; j++)
+////		{
+////			dist[i][j] = -1;
+////		}
+////		dist[i][i] = 1;
+////	}
+////	for (int i = 0; i < M; i++)
+////	{
+////		// scanf("%d%d", &a, &b);
+////		cin >> a >> b;
+////		nodelist[a].outlist.push_back(b);
+////		nodelist[b].inlist.push_back(a);
+////		dist[a][b] = 1;
+////	}
+////	for (int k = 1; k <= N; k++)
+////	{
+////		for (int i = 1; i <= nodelist[k].inlist.size(); i++)////每次求中间点标号不超过k的i到j最短路
+////		{
+////			for (int j = 1; j <= nodelist[k].outlist.size(); j++)
+////			{
+////				int index1 = nodelist[k].inlist[i - 1];
+////				int index2 = nodelist[k].outlist[j - 1];
+////				vector<int>::iterator it;
+////				it = find(nodelist[index1].outlist.begin(), nodelist[index1].outlist.end(), index2);
+////				if (it == nodelist[index1].outlist.end())
+////				{
+////					nodelist[index1].outlist.push_back(index2);
+////					nodelist[index2].inlist.push_back(index1);
+////				}
+////			}
+////		}
+////	}
+////	result = 0;
+////	for (int i = 1; i <= N; i++)
+////	{
+////		temp = 0;
+////		for (int j = 1; j <= N; j++)
+////		{
+////			vector<int>::iterator it1, it2;
+////			it1 = find(nodelist[i].outlist.begin(), nodelist[i].outlist.end(), j);
+////			it2 = find(nodelist[i].inlist.begin(), nodelist[i].inlist.end(), j);
+////			if (it1 != nodelist[i].outlist.end() || it2 != nodelist[i].inlist.end()) temp++;
+////		}
+////		if (temp >= N - 1) result++;
+////	}
+////	/*
+////	for (int k = 1; k <= N; k++)
+////	{
+////		for (int i = 1; i <= N; i++)////每次求中间点标号不超过k的i到j最短路
+////		{
+////			for (int j = 1; j <= N; j++)
+////			{
+////				if (dist[i][k] > 0 && dist[k][j] > 0)
+////					dist[i][j] = 1;
+////			}
+////		}
+////	}
+////	for (int i = 1; i <= N; i++)
+////	{
+////		temp = 0;
+////		for (int j = 1; j <= N; j++)
+////		{
+////			if (dist[i][j] > 0 || dist[j][i] > 0) temp++;
+////		}
+////		if (temp == N) result++;
+////	}
+////	*/
+////	printf("%d", result);
+////	return 0;
+////}
+//
+//// http://blog.csdn.net/zyy_1998/article/details/78334496
+//// 暴力搜图
+//// 遍历n个点（发现n才1000。。。），每次都将此时的节点当做起点，
+//// 然后开始查询通过此点能到达的所有点。
 //#include <stdio.h>
-//#include <vector>
+//#include <iostream>
 //#include <algorithm>
+//#include <string.h>
+//#include <vector>
 //using namespace std;
-//int dist[1010][1010] = { 0 };
-//struct node
+//int flag[1005][1005];
+//int vis[1005];
+//vector<int> V[1005];
+//
+//void dfs(int a, int t)
 //{
-//	vector<int> inlist;
-//	vector<int> outlist;
-//};
+//	vis[a] = 1;
+//	flag[a][t] = 1;
+//	flag[t][a] = 1;
+//	for (int i = 0; i < V[a].size(); i++)
+//	{
+//		if (!vis[V[a][i]])
+//		{
+//			dfs(V[a][i], t); // 已经遍历过的点就不要再遍历了
+//		}
+//	}
+//	return;
+//}
+//
 //int main()
 //{
 //	int N = 0, M = 0;
-//	int a = 0, b = 0;
-//	int result = 0;
-//	int temp = 0;
-//
 //	cin >> N >> M;
-//	node nodelist[1010];
-//	for (int i = 1; i <= N; i++)
+//	memset(flag, 0, sizeof(flag));
+//	for (int i = 0; i <= N; i++)
 //	{
-//		for (int j = 1; j <= N; j++)
-//		{
-//			dist[i][j] = -1;
-//		}
-//		dist[i][i] = 1;
+//		V[i].clear();
 //	}
-//	for (int i = 0; i < M; i++)
+//	int a, b;
+//	for (int i = 1; i <= M; i++)
 //	{
-//		// scanf("%d%d", &a, &b);
 //		cin >> a >> b;
-//		nodelist[a].outlist.push_back(b);
-//		nodelist[b].inlist.push_back(a);
-//		dist[a][b] = 1;
-//	}
-//	for (int k = 1; k <= N; k++)
-//	{
-//		for (int i = 1; i <= nodelist[k].inlist.size(); i++)////每次求中间点标号不超过k的i到j最短路
-//		{
-//			for (int j = 1; j <= nodelist[k].outlist.size(); j++)
-//			{
-//				int index1 = nodelist[k].inlist[i - 1];
-//				int index2 = nodelist[k].outlist[j - 1];
-//				vector<int>::iterator it;
-//				it = find(nodelist[index1].outlist.begin(), nodelist[index1].outlist.end(), index2);
-//				if (it == nodelist[index1].outlist.end())
-//				{
-//					nodelist[index1].outlist.push_back(index2);
-//					nodelist[index2].inlist.push_back(index1);
-//				}
-//			}
-//		}
-//	}
-//	result = 0;
-//	for (int i = 1; i <= N; i++)
-//	{
-//		temp = 0;
-//		for (int j = 1; j <= N; j++)
-//		{
-//			vector<int>::iterator it1, it2;
-//			it1 = find(nodelist[i].outlist.begin(), nodelist[i].outlist.end(), j);
-//			it2 = find(nodelist[i].inlist.begin(), nodelist[i].inlist.end(), j);
-//			if (it1 != nodelist[i].outlist.end() || it2 != nodelist[i].inlist.end()) temp++;
-//		}
-//		if (temp >= N - 1) result++;
-//	}
-//	/*
-//	for (int k = 1; k <= N; k++)
-//	{
-//		for (int i = 1; i <= N; i++)////每次求中间点标号不超过k的i到j最短路
-//		{
-//			for (int j = 1; j <= N; j++)
-//			{
-//				if (dist[i][k] > 0 && dist[k][j] > 0)
-//					dist[i][j] = 1;
-//			}
-//		}
+//		V[a].push_back(b);
 //	}
 //	for (int i = 1; i <= N; i++)
 //	{
-//		temp = 0;
-//		for (int j = 1; j <= N; j++)
-//		{
-//			if (dist[i][j] > 0 || dist[j][i] > 0) temp++;
-//		}
-//		if (temp == N) result++;
+//		memset(vis, 0, sizeof(vis));
+//		dfs(i, i);
 //	}
-//	*/
-//	printf("%d", result);
+//	int ans = 0, j = 0;
+//	for (int i = 1; i <= N; i++)
+//	{
+//		for (j = 1; j <= N; j++)
+//		{
+//			if (!flag[i][j] || !flag[j][i]) break;
+//		}
+//		if (j == N + 1) ans++;
+//	}
+//	cout << ans << endl;
 //	return 0;
 //}
